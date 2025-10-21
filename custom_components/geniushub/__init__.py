57d71f6982407a10877139b374dd51526635f194
RogerSelwyn <rogers@selwyn.org.uk>
2025-10-21 10:44:55 +0100

    maint: Add ability to reload

--- custom_components/geniushub/__init__.py
+++ custom_components/geniushub/__init__.py
@@ -84,6 +84,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: GeniusHubConfigEntry) ->
     return True
 
 
+async def async_unload_entry(hass: HomeAssistant, entry: GeniusHubConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
 def _create_hub_devices(
     hass: HomeAssistant, entry: GeniusHubConfigEntry, hub_uid: str, api: str, zone0: any
 ):
